9e45dba139c5129a5fe0205d35c47cf154ef9e8e
Thomas Mayer <thomas@residuum.org>
2015-11-09 00:40:42 +0100

    Drop if exists

--- scraper.py
+++ scraper.py
@@ -27,7 +27,7 @@ for row in dom.cssselect('tr.club-row'):
 
 if len(premierLeagueData) > 0:
     #truncate data store
-    scraperwiki.sql.execute("DELETE FROM `swdata`")
+    scraperwiki.sql.execute("DROP TABLE IF EXISTS `swdata`")
     #add each table line to data store
     for teamItem in premierLeagueData:
         scraperwiki.sql.save(['team'], teamItem)
